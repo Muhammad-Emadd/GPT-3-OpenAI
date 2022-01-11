86b2fd42c91487f7a3ccdbba10c595adee41daa7
3emo <mohamed.emadd91@gmail.com>
2022-01-11 13:58:26 +0200

    fixing layout

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -5,7 +5,7 @@ function Article({ imgUrl, date, title }) {
   return (
     <div className="gpt3__blog-container_article">
       <div className="gpt3__blog-container_article-image">
-        <img src={imgUrl} alt="blog imag" />
+        <img src={imgUrl} alt="blog" />
       </div>
       <div className="gpt3__blog-container_article-content">
         <div>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -31,7 +31,7 @@ function NavBar() {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} alt="logo image" />
+          <img src={logo} alt="logo" />
         </div>
 
         <div className="gpt3__navbar-links_container">
